ae329625be01840eb40d9ac9ab235831a3d69c6e
foufouboy <cissaclement@gmail.com>
2023-05-31 13:38:59 +0200

    finished newsletter component

---
+++ newsletter-component-main/main.js
@@ -0,0 +1,59 @@
+
+const FormValidator = (function() {
+
+    const showError = () => {
+        console.log("kkrrrr");  
+        emailInput.classList.add("invalid-email");
+        errorMsg.classList.remove("unactive");
+    }
+
+    const removeError = () => {
+        emailInput.classList.remove("invalid-email");
+        errorMsg.classList.add("unactive");
+    }
+
+    const isValidEmail = () => {
+        console.log(emailInput.validity.typeMismatch);
+        return !emailInput.validity.typeMismatch;
+    }
+
+    const isErrorShown = () => {
+        return !errorMsg.classList.contains("unactive"); 
+    }
+
+    const showSuccess = () => {
+        newsElt.classList.add("unactive");
+        successElt.classList.remove("unactive");
+        emailSub.textContent = emailInput.value;
+        
+    }
+
+    const submitButton = document.querySelector(".sign-up .subscribe-button");
+    const dismissButton = document.querySelector(".success-subscribe button");
+    const errorMsg = document.querySelector(".email-error");
+    const emailInput = document.querySelector("input");
+    const successElt = document.querySelector(".success-subscribe");
+    const newsElt = document.querySelector(".sign-up");
+    const emailSub = document.querySelector(".sub-mail");
+
+    dismissButton.addEventListener("click", () => {
+        window.location.href = "/"; 
+    });
+
+    submitButton.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        if (isValidEmail()) {
+            showSuccess();
+        } else {
+            showError();
+        } 
+    });
+
+    emailInput.addEventListener("input", () => {
+        if (isErrorShown()) {
+            removeError();
+        }
+    });
+
+})();
